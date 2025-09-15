5cba154462f3e81a8e515ce475cdb8dfd6d25091
Developer <developer@example.com>
2025-09-15 22:29:52 +0100

    Initial commit before UI overhaul

---
+++ build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_AmineTemplar_FILE C:/Users/user/Documents/QT - Test/AmineTemplar/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/AmineTemplar.exe)
+set(__QT_DEPLOY_TARGET_AmineTemplar_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_AmineTemplar_RUNTIME_DLLS C:/Qt/6.7.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Core.dll)
